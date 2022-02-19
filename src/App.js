5f423360f4ccc42a53b19107f102baa9e812ae53
Karthik M R <karthik.m-r@hpe.com>
2022-02-19 21:29:30 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          hello karthik <code>src/App.js</code> and hope you are doing well!!
         </p>
         <a
           className="App-link"
